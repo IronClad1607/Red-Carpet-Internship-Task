26a1bf071a8301d55121490bc1f23766b9ad2a48
Ishaan Jain <ishaan160799@gmail.com>
2020-09-29 06:35:09 +0530

    splash activity done

---
+++ app/src/main/java/com/ishaanjain/redcarpetinternship/ui/view/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.ishaanjain.redcarpetinternship.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ishaanjain.redcarpetinternship.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
--- app/src/main/java/com/ishaanjain/redcarpetinternship/ui/view/SplashActivity.kt
+++ app/src/main/java/com/ishaanjain/redcarpetinternship/ui/view/SplashActivity.kt
@@ -1,12 +1,22 @@
 package com.ishaanjain.redcarpetinternship.ui.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.ishaanjain.redcarpetinternship.R
 
 class SplashActivity : AppCompatActivity() {
+
+    private val splashDelay: Long = 3000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val splashIntent = Intent(this, HomeActivity::class.java)
+            startActivity(splashIntent)
+        }, splashDelay)
     }
 }
